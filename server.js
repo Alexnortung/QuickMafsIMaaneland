b8493861079ead6cfbce729c3620bf2d13c66e4f
Nicolas <nicolas.dyhrman@gmail.com>
2018-03-02 14:10:13 +0100

    Update Socket.io
    Personal right now

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var port = process.env.PORT || 3000;
 var mysql = require('mysql');
 var io = require('socket.io')(http);
 var sqlS = require('./sqlSetup.js');
 
 app.use("/", express.static(__dirname + "/game"));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+sqlS.SetupMySql(mysql);
+var con = sqlS.CreateNewCon(mysql);
+sqlS.CreateUserTable(con);
+sqlS.CreateMatchesTable(con);
 
 io.on('connection', function(socket)
 {
@@ -19,7 +23,6 @@ io.on('connection', function(socket)
   });
 });
 
-sqlS.SetupMySql(mysql);
-var con = sqlS.CreateNewCon(mysql);
-sqlS.CreateUserTable(con);
-sqlS.CreateMatchesTable(con);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
