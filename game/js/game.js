c56202bf6b64a30583134d842fb0961f0ad89076
Alexnortung <alex_nortung@live.dk>
2018-03-05 22:25:11 +0100

    beginning canvas

--- game/js/game.js
+++ game/js/game.js
@@ -1,4 +1,48 @@
 $(function () {
-    var socket = io();
+    //var socket = io();
 
   });
+
+
+var mgr;
+
+function setup() {
+	var c = createCanvas(600,500);
+
+	$(c.elt.id).appendTo("#canvasContainer");
+	mgr = new SceneManager();
+
+	mgr.addScene(MainMenu);
+
+	mgr.showNextScene();
+
+}
+
+function draw() {
+	mgr.draw();
+}
+
+
+function MainMenu() {
+	this.setup = function(){
+
+
+
+	}
+
+	this.draw = function() {
+		//background
+		background(50);
+
+		//queue for match button
+		//rect
+
+		//text
+	}
+
+
+	
+
+}
+
+
