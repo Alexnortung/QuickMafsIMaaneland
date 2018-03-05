07e2846162bae22b7bb6471ddf633bcb49ae6c43
Alexander Nortung <alex_nortung@liuve.dk>
2018-03-05 18:02:39 +0100

    Created new functions for controlling the game

--- game/js/game.js
+++ game/js/game.js
@@ -1,6 +1,9 @@
 var c;
 var ctx;
 var socket;
+var currentGame;
+var findingGame = false;
+var ingame = false;
 
 $(function ()
 {
@@ -18,3 +21,65 @@ function changeCanvasPosition()
   c.style.marginRight = ((window.width/2) - (c.width/2)) + "px";
   c.style.marginTop = ((window.height/2) - (c.height/2)) + "px";
 }
+
+
+function findGame() {
+	socket.emit("findMathGame");
+}
+
+function onGameFound(data) {
+	//data should be a JSON object containing:
+	//questions: an array of question objects
+
+	if (typeof data == "undefined") {
+		console.log("data is undefined");
+		return;
+	} else if (typeof data == "string") {
+		try {
+			data = JSON.parse(data);
+		} catch(e){
+			console.log("couldn't parse data on foundgame response");
+			return;
+		}
+	}
+
+	if (typeof data != "object") {
+		console.log("foundgame response is not an object", data);
+		return;
+	}
+	if (typeof data.questions != "object") {
+		console.log("foundgame response .queations is not an array");
+	}
+
+	currentGame = new Game(data.questions);
+	//change scene to Game
+
+
+}
+
+function sendChallenge(friendId) {
+	socket.emit("challenge", {friendId: friendId});
+}
+
+function onChallengeResponse(data) {
+	//data should be an object with an accepted key as a boolean of the other payer accepted  
+}
+
+
+
+
+
+
+
+
+function Game(questions){
+	this.questions = questions;
+}
+
+Game.prototype.getGameState = function() {
+	//emit to the server that 
+};
+
+
+
+
