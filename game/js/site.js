22c649338a5e0db2e8dfb85e523a10de701c5b6c
nicolas gram dyhrman <nicolas.dyhrman@gmail.com>
2018-03-08 23:57:11 +0100

    Server and client Login
    Created updates about login on both client and server login

--- game/js/site.js
+++ game/js/site.js
@@ -1,53 +1,83 @@
-$(function ()
-{
-  $( "#login" ).click(function()
-  {
+$(function() {
+  $("#login").click(function() {
     TurnOverLayOn();
+    LoginScreen();
     console.log("Login");
   });
 
-  $("#register").click(function()
-  {
+  $("#register").click(function() {
     TurnOverLayOn();
+    RegisterScreen();
     console.log("Register");
   });
 
-  $("#registerButton").click(function()
-  {
+  $("#registerButton").click(function() {
     Register();
   });
 
-  socket.on("registerResult", function(msg)
-  {
+  $("#loginButton").click(function() {
+    Login();
+    console.log("Login initiated");
+  });
+
+  socket.on("registerResult", function(msg) {
     console.log("Register Status: " + msg);
+
+  });
+
+  socket.on("LoggedMenu", function(username)
+  {
+    console.log("Overlay Removal initiated");
+    $("#overlay").remove();
+    //Remove other
+    //Place logout loginButton
+    //Write "Hello, " + username
+  });
+
+  $(".CloseOverlay").click(function()
+  {
+    TurnOffOverlay();
   });
 });
 
-function TurnOverLayOn()
+function TurnOffOverlay()
 {
+  $("#overlay").hide();
+  $("#loginOverlay").hide();
+  $("#registerOverlay").hide();
+}
+
+function TurnOverLayOn() {
   $("#overlay").show();
 }
 
-function LoginScreen()
-{
+function LoginScreen() {
   $("#loginOverlay").show();
 }
 
-function RegisterScreen()
-{
+function RegisterScreen() {
   $("#registerOverlay").show();
 }
 
-function Register()
-{
+function Register() {
   console.log("Register initiated");
-  var registerObject =
-  {
+  var registerObject = {
     username: $("#username").val(),
-    passowrd: $("#password").val(),
+    password: $("#password").val(),
     displayname: $("#displayname").val(),
     email: $("#email").val()
   };
-
   socket.emit('register', registerObject);
+}
+
+function GetLoginCredentials() {
+  var LoginObject = {
+    username: $("#loginusername").val(),
+    password: $("#loginpassword").val()
+  };
+  return LoginObject;
+}
+
+function Login() {
+  socket.emit('login', GetLoginCredentials());
 }
