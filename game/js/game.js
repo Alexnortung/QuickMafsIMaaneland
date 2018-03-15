e375c2d71d38fd9a81792af0f0c96f19add222c0
Alexander Nortung <alex_nortung@liuve.dk>
2018-03-15 17:59:09 +0100

    started math game 'class'

--- game/js/game.js
+++ game/js/game.js
@@ -17,6 +17,7 @@ $(function ()
 var mgr;
 
 function setup() {
+	//p5 function
 	console.log("henlo world!");
 	c = createCanvas(1920*0.6,1080*0.6);
 	
@@ -26,13 +27,20 @@ function setup() {
 	mgr = new SceneManager();
 
 	mgr.addScene(MainMenu);
+	mgr.addScene(GameScene);
 
 	mgr.showNextScene();
 
 }
 
 function draw() {
+	//p5 function
 	mgr.draw();
+}
+
+function mousePressed() {
+	//p5 function
+	mgr.scene.oScene.mousePress();
 }
 
 
@@ -47,39 +55,65 @@ function getSize(scale, direction){
 
 function MainMenu() {
 	this.setup = function(){
+		this.findingMatch = false;
 		this.bg = loadImage("img/MånebyLockedandLoadedExpandedVersion.png");
 		this.queueBut = loadImage("img/Queue.png");
 		//create click regions
-		this.findMatchRegion = new Region(getSize(0.7, 0), getSize(0.75, 1), getSize(0.2, 0), getSize(0.175, 1), findGame);
+		this.findMatchRegion = new Region(getSize(0.7, 0), getSize(0.75, 1), getSize(0.2, 0), getSize(0.175, 1));
+		this.findMatchRegion.onclick = function () {
+			findGame();
+		}
+		this.cancelMatchRegion = new Region(getSize(0.7, 0), getSize(0.75, 1), getSize(0.2, 0), getSize(0.175, 1));
+		this.cancelMatchRegion.onClick = function () {
+			
+		}
+
+		this._mouseHandler = new MouseHandler();
+		this._mouseHandler.addRegion(this.findMatchRegion);
+
+		
 
 
 
 	}
+
+
 
 	this.draw = function() {
 		//background
 		background(this.bg);
 		
 
-		//queue for match button
+		//queue for match button else cancel box
 		//box
 		push();
 		var r = this.findMatchRegion;
-		r.isInside() ? fill(28, 101, 219) : fill(0, 128, 43);
+		if (!this.findingMatch) {
+			r.isInside() ? fill(28, 101, 219) : fill(0, 128, 43);
+			rect(r.x, r.y, r.w, r.h, 20);
+			pop();
+			push();
+			textSize(getSize(0.05, 1));
+			text("Find Match", getSize(0.735, 0), getSize(0.85, 1));
+		}else {
+			r.isInside() ? fill(219, 206, 28) : fill(219, 37, 28);
+			rect(r.x, r.y, r.w, r.h, 20);
+			pop();
+			push();
+			textSize(getSize(0.05, 1));
+			text("Cancel", getSize(0.735, 0), getSize(0.85, 1));
+		}
 		
-		
-
-		rect(r.x, r.y, r.w, r.h, 20);
 		pop();
 		//text
-		push();
-		textSize(getSize(0.05, 1));
-		text("Find Match", getSize(0.735, 0), getSize(0.85, 1));
-		pop();
 
 	}
-
 }
+
+MainMenu.prototype.mousePress = function () {
+	this._mouseHandler.onClick(mouseX, mouseY);
+}
+
 
 
 
@@ -103,6 +137,9 @@ function GameScene() {
 
 }
 
+GameScene.prototype.mousePress = function () {
+	this._mouseHandler.onClick(mouseX, mouseY);
+}
 
 
 
@@ -113,43 +150,108 @@ function changeCanvasPosition()
 }
 
 
+function cancelGame() {
+	//sends to the serber a request to cancel the game
+	socket.emit("cancelFindMatch");
+}
+
+function onCancelMatchResponse(data) {
+	/*data is an object that has:
+	status: Boolean (true if the player is no longer in a queue)
+	//*/
+
+	var d1 = checkData(data);
+	if (d1 == false) {
+		return;
+	}
+
+	if (d1.status) {
+		var tScene = mgr.scene.oScene
+		//in queue
+		tScene.findingMatch = false;
+		//change region from find match to cancel search
+		tScene._mouseHandler.removeRegion(tScene.cancelMatchRegion.id);
+		tScene._mouseHandler.addRegion(tScene.findMatchRegion);
+
+	}
+
+}
+
+
 
 function findGame() {
+	//console.log("sending request for finding a game");
+	//sends a request to find a game
 	socket.emit("findMathGame");
 }
 
-function onGameFound(data) {
-	//data should be a JSON object containing:
-	//questions: an array of question objects
+function onFindMatchResponse(data) {
+	/*data is an object that has:
+	status: Boolean (true if the player is now in a queue)
+	//*/
+	var d1 = checkData(data);
+	if (d1 == false) {
+		return;
+	}
 
+	if (d1.status) {
+		var tScene = mgr.scene.oScene
+		//in queue
+		tScene.findingMatch = true;
+		//change region from find match to cancel search
+		tScene._mouseHandler.removeRegion(tScene.findMatchRegion.id);
+		tScene._mouseHandler.addRegion(tScene.cancelMatchRegion);
+
+	}
+
+}
+
+function checkData(data) {
 	if (typeof data == "undefined") {
 		console.log("data is undefined");
-		return;
+		return false;
 	} else if (typeof data == "string") {
 		try {
 			data = JSON.parse(data);
 		} catch(e){
-			console.log("couldn't parse data on foundgame response");
-			return;
+			console.log("couldn't parse data from response", e);
+			return false;
 		}
 	}
 
 	if (typeof data != "object") {
-		console.log("foundgame response is not an object", data);
+		console.log("response is not an object", data);
+		return false;
+	}
+
+	return data;
+
+}
+
+
+function onGameFound(data) {
+	//data should be a JSON object containing:
+	//questions: an array of question objects
+	var d1 = checkData(data);
+	if (d1 == false) {
 		return;
 	}
-	if (typeof data.questions != "object") {
+	
+	if (typeof d1.questions != "object") {
 		console.log("foundgame response .queations is not an array");
 	}
 
-	currentGame = new Game(data.questions);
+	currentGame = new Game(d1.questions);
 	//change scene to Game
-
 
 }
 
 function sendChallenge(friendId) {
 	socket.emit("challenge", {friendId: friendId});
+}
+
+function onChallengeRecieved(data) {
+	// body...
 }
 
 function onChallengeResponse(data) {
@@ -165,7 +267,26 @@ function onConnectAndGame(){
 
 
 
-function MathGame(){
+function MathGame(questionLength, callback){
+	this.player0 = new MathGamePlayer();
+	this.player1 = new MathGamePlayer();
+	this.players = [player0, player1];
+	this.questionLength = questionLength;
+	this.callback = callback;
+}
+
+MathGame.prototype.addProgress = function (playerInt) {
+	this.players[playerInt].addProgress();
+	if (this.players[playerInt].progress == this.questionLength) {
+		//this player has won!
+		//end the game by telling who won the game
+
+		//start winner animation
+
+		//run callback function
+		this.callback();
+	}
+
 
 }
 
@@ -176,7 +297,13 @@ function MathGame(){
 
 
 
+function MathGamePlayer() {
+	this.progress = 0;
+}
 
+MathGamePlayer.prototype.addProgress = function() {
+	this.progress++;
+};
 
 
 
@@ -190,6 +317,7 @@ function Region(x,y,width, height) {
 }
 
 Region.prototype.isInside = function() {
+	//check the position of the mouse relative to the region.
 	if (this.x > mouseX) {
 		return false;
 	}
@@ -214,19 +342,22 @@ function MouseHandler() {
 	this.rID = 0
 }
 
-ClickHandler.prototype.addRegion = function(region) {
-	var regionId = this.getNewRID;
-	region.id = regionId;
+MouseHandler.prototype.addRegion = function(region) {
+	if (typeof region.id == "undefined") {
+		var regionId = this.getNewRID;
+		region.id = regionId;
+	}
+	
 	this.regions.push(region);
-	return regionId;
+	return region;
 };
 
-ClickHandler.prototype.getNewRID = function(){
+MouseHandler.prototype.getNewRID = function(){
 	this.rID++;
 	return this.rID - 1;
 }
 
-ClickHandler.prototype.removeRegion = function(id) {
+MouseHandler.prototype.removeRegion = function(id) {
 	//finding region with that id
 	for (var i = this.regions.length - 1; i >= 0; i--) {
 		if(this.regions[i].id == id){
@@ -236,25 +367,14 @@ ClickHandler.prototype.removeRegion = function(id) {
 	}
 };
 
-ClickHandler.prototype.onClick = function(x,y) {
+MouseHandler.prototype.onClick = function(x,y) {
 	//check all regions if the click was inside one of them
 	for (var i = this.regions.length - 1; i >= 0; i--) {
-		if (!this.regions[i].x < x) {
-			continue;
+		if (typeof this.regions[i].onclick == "function") {
+			if (this.regions[i].isInside()) {
+				this.regions[i].onclick();
+			}
 		}
-		if (!(this.regions[i].x + this.regions[i].width) > x ) {
-			continue;
-		}
-		if (!this.regions[i].y < y) {
-			continue;
-		}
-		if (!(this.regions[i].y + this.regions[i].height) > y ) {
-			continue;
-		}
-
-		
-
-		
 	}
 };
 
